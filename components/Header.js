8628e8fcbe801c73597c0568c861d72f72fcb9fe
tansionline <tatarselcuk27@gmail.com>
2021-05-11 21:07:36 +0300

    Logos, favicon updated, Components folder and about file created :nail_care:

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className="container mx-auto p-12" id="headerBackground">
+      <div>
+        <img
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+          width={300}
+          height={300}
+          alt="logo"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
-export default function Home() {
+export default function About() {
   return (
     <div>
       <Head>
-        <title>Movie Database</title>
-        <meta name="description" content="Movie Database App" />
+        <title>About</title>
+        <meta name="description" content="Movie Database App About Page" />
         <link
           rel="icon"
           href="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
