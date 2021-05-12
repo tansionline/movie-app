dd98115277c7f90dc27281ab7785acb19aa827ba
tansionline <tatarselcuk27@gmail.com>
2021-05-12 03:24:58 +0300

    next.config.js deleted

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/tansionline/movie-app/master/"
-    : "",
-};
